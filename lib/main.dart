fb2d90b7c70a612368d777d7b2eb4879fbe0b979
Werner1201 <werner.romling@unigranrio.br>
2019-10-22 16:53:33 -0300

    Removendo Itens de Fato e cancelando a remocao

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,8 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   List _toDoList = [];
   final _toDoController = TextEditingController();
-
+  Map<String, dynamic> _lastRemoved;
+  int _lastRemovedPos;
 
   @override
   void initState() {
@@ -130,27 +131,35 @@ class _HomeState extends State<Home> {
           });
         },
       ),
+      onDismissed: (direction){
+       setState(() {
+         _lastRemoved = Map.from(_toDoList[index]);
+         _lastRemovedPos = index;
+         _toDoList.removeAt(index);
+         _saveData();
+
+         final snack = SnackBar(
+           content: Text("Tarefa \"${_lastRemoved["title"]}\" removida!"),
+           action: SnackBarAction(
+             label: "Desfazer",
+             onPressed: (){
+               setState(() {
+                 _toDoList.insert(_lastRemovedPos, _lastRemoved);
+                 _saveData();
+               });
+             },
+           ),
+           duration: Duration(seconds: 2),
+         );
+
+         Scaffold.of(context).showSnackBar(snack);
+
+       });
+
+      },
     );
   }
-/*
-  CheckboxListTile(
-  title: Text(_toDoList[index]["title"]),
-  value: _toDoList[index]["ok"],
-  secondary: CircleAvatar(
-  child: Icon(
-  // Se o ok for true vai marcar se nao vai deixar sem
-  _toDoList[index]["ok"]? Icons.check : Icons.error
-  ),
-  ),
-  onChanged: (c){
-  setState(() {
-  _toDoList[index]["ok"] = c;
-  _saveData();
-  });
-  },
-  );
 
-*/
   //Pega um diretorio para armazenar o arquivo de dados
   Future<File> _getFile() async {
     final directory = await getApplicationDocumentsDirectory();
